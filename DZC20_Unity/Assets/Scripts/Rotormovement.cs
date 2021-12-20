5767c1ef6cfa06f4f629214d0fc00ea550b992fc
AlexFris <94806073+AlexFris@users.noreply.github.com>
2021-12-20 12:29:35 +0100

    Added the main drone
    the main drone is now located in the drone scene

---
+++ DZC20_Unity/Assets/Scripts/Rotormovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotormovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float PropSpeed = 300f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, PropSpeed * Time.fixedDeltaTime, 0f, Space.Self);
+    }
+}
