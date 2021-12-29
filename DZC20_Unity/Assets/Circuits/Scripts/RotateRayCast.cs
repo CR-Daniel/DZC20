b89588982fce5101f181c91400f7e624e97cbb39
youssef selim <y.selim@bluejayeindhoven.nl>
2021-12-29 11:29:44 +0200

    testing circuit code

---
+++ DZC20_Unity/Assets/Circuits/Scripts/RotateRayCast.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRayCast : MonoBehaviour
+{
+    private void Update(){
+        if(Input.GetMouseButtonDown(0)){
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, 100.0f)){
+
+                if(hit.transform != null){
+                    PrintName(hit.transform.gameObject);
+                    //Rigidbody rb;
+                    //if(rb = hit.transform.GetComponent<Rigidbody>()){
+                        
+                      //  Rotate(rb);
+                    //}
+                }
+            }
+        }  
+    }
+
+    private void PrintName(GameObject go){
+        print(go.name);
+    }
+
+    //private void Rotate(Rigidbody rig){
+      //  rig.transform.Rotate(Vector3.up, 90.0f);
+    //}
+}
