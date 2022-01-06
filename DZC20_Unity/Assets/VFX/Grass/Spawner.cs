2b6b13c0721ef6aa751e0fa069bc2e0e1dda76c5
daniel <d.chou.rainho@student.tue.nl>
2022-01-06 20:29:43 +0100

    (Atlas) Placing Grass and Water
    Finishing the first half of the maze.

---
+++ DZC20_Unity/Assets/VFX/Grass/Spawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Spawner : MonoBehaviour
+{
+    public bool spawn;
+    public GameObject ground;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawn){
+            spawn = false;
+
+            
+
+
+        }
+    }
+}
