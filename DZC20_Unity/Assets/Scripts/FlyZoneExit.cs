f4c70f78b4fb63010d603df206717b96398e064b
daniel <d.chou.rainho@student.tue.nl>
2022-01-13 17:22:12 +0100

    (TUe) Added UI Messages when trying to exit Fly Zone.

---
+++ DZC20_Unity/Assets/Scripts/FlyZoneExit.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyZoneExit : MonoBehaviour
+{
+    public GameObject error;
+    public Text text;
+    private int messageIndex = 0;
+    private string[] messages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        messages = new string[] {
+            "What are you doing? Return to campus!",
+            "Trying to escape the campus again ey?",
+            "You think this is funny huh?!",
+            "...",
+            "FINE, JUST LEAVE THEN!"
+        };
+        error.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        // Show Error Message
+        StartCoroutine(ShowError(collider));
+    }
+
+    IEnumerator ShowError(Collider collider)
+    {
+        text.text = messages[messageIndex];
+        messageIndex += 1;
+
+        if(messageIndex == messages.Length){
+            Destroy(collider);
+        }
+
+        error.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        error.SetActive(false);
+    }
+}
