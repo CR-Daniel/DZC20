96024425e2f03b30a82eefd5fa721bbf25d3f23c
daniel <d.chou.rainho@student.tue.nl>
2022-01-15 20:14:04 +0100

    (Flux) Added Camera Switch Script

---
+++ DZC20_Unity/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera[] cameras;
+    private int levelIndex = 0;
+
+    void Start()
+    {
+        // Disable All Cameras
+        foreach (Camera cam in cameras){
+            cam.enabled = false;
+        }
+
+        // Enable First Camera
+        cameras[0].enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // Disable Current Camera
+            cameras[levelIndex].enabled = false;
+
+            // Get Next Camera
+            if (levelIndex + 1 != cameras.Length){
+                levelIndex += 1;
+            }
+
+            // Enable Next Camera
+            cameras[levelIndex].enabled = true;
+        }
+    }
+}
