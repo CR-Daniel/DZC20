1d2732de67e889e3783f7c97cdcc194a571f7933
daniel <d.chou.rainho@student.tue.nl>
2022-01-16 20:43:40 +0100

    (Flux) Added Level Transition Screens

--- DZC20_Unity/Assets/Circuits/Scripts/RotateRayCast.cs
+++ DZC20_Unity/Assets/Circuits/Scripts/RotateRayCast.cs
@@ -6,24 +6,26 @@ public class RotateRayCast : MonoBehaviour
 {
     public const float MOVEMENT_TIME = 2.0f; // The object moves for 2 seconds
     public const float MOVEMENT_DISTANCE = 40.0f; // Distance the object should move
+
     //Level 1 SphereX
-    public Transform Level1X;
-    //Level 2 SphereX
-    public Transform Level2X;
-    //Level 2 SphereY
-    public Transform Level2Y;
-    //Level 3 SphereX
-    public Transform Level3X;
-    //Level 4 SphereX
-    public Transform Level4X;
-    //Level 4 SphereY
-    public Transform Level4Y;
-    //Level 4 SphereZ
-    public Transform Level4Z;
-    //Level 4 SphereW
-    public Transform Level4W;
-    //Level 4 SphereV
-    public Transform Level4V;
+    // public Transform Level1X;
+    // //Level 2 SphereX
+    // public Transform Level2X;
+    // //Level 2 SphereY
+    // public Transform Level2Y;
+    // //Level 3 SphereX
+    // public Transform Level3X;
+    // //Level 4 SphereX
+    // public Transform Level4X;
+    // //Level 4 SphereY
+    // public Transform Level4Y;
+    // //Level 4 SphereZ
+    // public Transform Level4Z;
+    // //Level 4 SphereW
+    // public Transform Level4W;
+    // //Level 4 SphereV
+    // public Transform Level4V;
+    
     public GameObject camera;
     public GameObject EndCam;
     private void Update(){
@@ -45,35 +47,35 @@ public class RotateRayCast : MonoBehaviour
                     }
                 }
             }
-        //Level 1
-        if(Level1X.eulerAngles.z >= 180f && Level1X.eulerAngles.z <= 180f){
-            camera.SetActive(false);
-            EndCam.SetActive(true);
-        }
-        //Level 2
-        if((Level2X.eulerAngles.z >= 180f && Level2X.eulerAngles.z <= 180f) && (
-            (Level2Y.eulerAngles.z >= 180f && Level2Y.eulerAngles.z <= 180f) ||
-            (Level2Y.eulerAngles.z >= 90f && Level2Y.eulerAngles.z <= 90f)))
-        {
-            camera.SetActive(false);
-            EndCam.SetActive(true);
-        }
-        //Level 3
-        if((Level3X.eulerAngles.z >= 0f && Level3X.eulerAngles.z <= 0f) || 
-            (Level3X.eulerAngles.z >= 270f && Level3X.eulerAngles.z <= 270f)){
-            camera.SetActive(false);
-            EndCam.SetActive(true);
-            // StopCoroutine("MoveObject");
-            // StartCoroutine("MoveObject", MOVEMENT_DISTANCE);
-        }
-        //Level 4
-        if((Level4X.eulerAngles.z >= 0f && Level4X.eulerAngles.z <= 0f) && 
-            (Level4Z.eulerAngles.z >= 90f && Level4Z.eulerAngles.z <= 90f) &&
-            (Level4W.eulerAngles.z >= 270f && Level4W.eulerAngles.z <= 270f) &&
-            (Level4V.eulerAngles.z >= 180f && Level4V.eulerAngles.z <= 180f)){
-            camera.SetActive(false);
-            EndCam.SetActive(true);
-        }
+        // //Level 1
+        // if(Level1X.eulerAngles.z >= 180f && Level1X.eulerAngles.z <= 180f){
+        //     camera.SetActive(false);
+        //     EndCam.SetActive(true);
+        // }
+        // //Level 2
+        // if((Level2X.eulerAngles.z >= 180f && Level2X.eulerAngles.z <= 180f) && (
+        //     (Level2Y.eulerAngles.z >= 180f && Level2Y.eulerAngles.z <= 180f) ||
+        //     (Level2Y.eulerAngles.z >= 90f && Level2Y.eulerAngles.z <= 90f)))
+        // {
+        //     camera.SetActive(false);
+        //     EndCam.SetActive(true);
+        // }
+        // //Level 3
+        // if((Level3X.eulerAngles.z >= 0f && Level3X.eulerAngles.z <= 0f) || 
+        //     (Level3X.eulerAngles.z >= 270f && Level3X.eulerAngles.z <= 270f)){
+        //     camera.SetActive(false);
+        //     EndCam.SetActive(true);
+        //     // StopCoroutine("MoveObject");
+        //     // StartCoroutine("MoveObject", MOVEMENT_DISTANCE);
+        // }
+        // //Level 4
+        // if((Level4X.eulerAngles.z >= 0f && Level4X.eulerAngles.z <= 0f) && 
+        //     (Level4Z.eulerAngles.z >= 90f && Level4Z.eulerAngles.z <= 90f) &&
+        //     (Level4W.eulerAngles.z >= 270f && Level4W.eulerAngles.z <= 270f) &&
+        //     (Level4V.eulerAngles.z >= 180f && Level4V.eulerAngles.z <= 180f)){
+        //     camera.SetActive(false);
+        //     EndCam.SetActive(true);
+        // }
     }
 
     private void PrintName(GameObject go){
--- DZC20_Unity/Assets/Scripts/CameraSwitch.cs
+++ DZC20_Unity/Assets/Scripts/CameraSwitch.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraSwitch : MonoBehaviour
 {
+    // UI Elements
+    public GameObject button;
+    public GameObject[] screens;
+
     public Camera[] cameras;
     private int levelIndex = 0;
+
+    // Collection Of Spheres
+    public Transform[] level1;
+    public Transform[] level2;
+    public Transform[] level3;
+    public Transform[] level4;
+
 
     void Start()
     {
@@ -16,10 +28,69 @@ public class CameraSwitch : MonoBehaviour
 
         // Enable First Camera
         cameras[0].enabled = true;
+
+        // Disable All UI Screens
+        foreach (GameObject screen in screens){
+            screen.SetActive(false);
+        }
+
+        // Disable Button
+        button.SetActive(false);
+    }
+
+    void Update()
+    {
+        switch(levelIndex) {
+            case 0:
+                bool a1 = level1[0].eulerAngles.z >= 180f && level1[0].eulerAngles.z <= 180f;
+
+                if(a1){
+                    screens[levelIndex].SetActive(true);
+                    button.SetActive(true);
+                }
+                break;
+
+            case 1:
+                bool b1 = level2[0].eulerAngles.z >= 180f && level2[0].eulerAngles.z <= 180f;
+                bool b2 = level2[1].eulerAngles.z >= 180f && level2[1].eulerAngles.z <= 180f;
+                bool b3 = level2[1].eulerAngles.z >= 90f && level2[1].eulerAngles.z <= 90f;
+
+                if (b1 && (b2 || b3)){
+                    screens[levelIndex].SetActive(true);
+                    button.SetActive(true);
+                }
+                break;
+
+            case 2:
+                bool c1 = level3[0].eulerAngles.z >= 0f && level3[0].eulerAngles.z <= 0f;
+                bool c2 = level3[0].eulerAngles.z >= 270f && level3[0].eulerAngles.z <= 270f;
+
+                if(c1 || c2){
+                    screens[levelIndex].SetActive(true);
+                    button.SetActive(true);
+                }
+                break;
+
+            case 3:
+                bool d1 = level4[0].eulerAngles.z >= 0f && level4[0].eulerAngles.z <= 0f;
+                bool d2 = level4[2].eulerAngles.z >= 90f && level4[2].eulerAngles.z <= 90f;
+                bool d3 = level4[3].eulerAngles.z >= 270f && level4[3].eulerAngles.z <= 270f;
+                bool d4 = level4[4].eulerAngles.z >= 180f && level4[4].eulerAngles.z <= 180f;
+
+                if (d1 && d2 && d3 && d4){
+                    screens[levelIndex].SetActive(true);
+                    button.SetActive(true);
+                }
+                break;
+        }
     }
 
     public void Switch()
     {
+        // Hide UI Screen
+        screens[levelIndex].SetActive(false);
+        button.SetActive(false);
+        
         // Disable Current Camera
         cameras[levelIndex].enabled = false;
 
